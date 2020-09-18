bdac1d70042ccf9a43e00f64a5991e4689f7346d
pellok2002 <pellok2002@gmail.com>
2020-09-18 17:44:34 +0800

    first commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PellokITHomeAccount.Data;
+
+[assembly: HostingStartup(typeof(PellokITHomeAccount.Areas.Identity.IdentityHostingStartup))]
+namespace PellokITHomeAccount.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
